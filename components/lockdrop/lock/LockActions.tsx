22c7fbe37a5c6f75080348716742799f06d96982
Charly Bernard <charly.bernard@gmail.com>
2021-12-10 06:52:53 +0100

    fix(lockActions): ASTRO-421

--- components/lockdrop/lock/LockActions.tsx
+++ components/lockdrop/lock/LockActions.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Button } from "@chakra-ui/react";
+
+import NextLink from "next/link";
+
+import CloseIcon from "components/icons/CloseIcon";
 
 const LockActions: FC = () => {
   return (
-    <Flex justify="space-between" color="white" mb="4" px="6">
+    <Flex justify="space-between" align="center" color="white" mb="4" px="6">
       <Box flex="1">
         <Text fontSize="xl" color="white">
           Lock your Terraswap LP tokens in Astroport
         </Text>
       </Box>
+      <NextLink href="/active-phase" passHref>
+        <Button variant="icon">
+          <CloseIcon w="6" h="6" color="white" />
+        </Button>
+      </NextLink>
     </Flex>
   );
 };
