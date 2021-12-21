a1ce8dc9b1486bf0c15def6374f368dbd83f4a90
Charly Bernard <charly.bernard@gmail.com>
2021-12-21 06:28:08 +0100

    feat(timeline): Highlight "Astroport Launch" when phase 2 countdown hits zero (#25)

--- components/pages/Phase2.tsx
+++ components/pages/Phase2.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { VStack, Link } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import dayjs from "dayjs";
 
-import Notification from "components/Notification";
+import { useAstroApp } from "modules/common";
+
 import LaunchTimeline from "components/LaunchTimeline";
 import Phase2Bootstrap from "components/Phase2Bootstrap";
 import AddLiquidity from "components/AddLiquidity";
 import AstroAirdrop from "components/AstroAirdrop";
 
 const Phase2 = () => {
+  const { phase2EndDate } = useAstroApp();
+  const phase2EndTimestamp = phase2EndDate?.unix();
+  const timestamp = dayjs().unix();
+  const currentPhase = phase2EndTimestamp < timestamp ? 3 : 2;
+
   return (
     <VStack my="12" spacing="16" align="stretch">
       {/* <Notification variant="info">
@@ -22,7 +29,7 @@ const Phase2 = () => {
         Thank you for participating in phase 1, 3,000 ASTRO were added to your
         ASTRO Balance. <Link>Learn More</Link>
       </Notification> */}
-      <LaunchTimeline phase={2} />
+      <LaunchTimeline phase={currentPhase} />
       <Phase2Bootstrap />
       <AddLiquidity />
       <AstroAirdrop />
