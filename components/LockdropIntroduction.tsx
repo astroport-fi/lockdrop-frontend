2a150ff8446b61c6601a191d1f8f20ad6525c76c
block902 <bruceli829@gmail.com>
2022-04-28 05:49:44 -0400

    feat: add update lock drop overview section

--- components/LockdropIntroduction.tsx
+++ components/LockdropIntroduction.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Image } from '@chakra-ui/react';
+import ApolloCountdown from './ApolloCountdown';
 
 const LockdropIntroduction = () => {
   return (
     <Box>
       <Box position="relative" borderRadius="15px" overflow="hidden">
-        <Image src="/hero_image_background.png" alt="lockdrop banner" />
+        <Image src="/hero_image_background.png" alt="lockdrop background" />
         <Box position="absolute" bottom={0}>
           <Image src="/hero_image_characters.png" alt="lockdrop banner" />
+        </Box>
+      </Box>
+      <Box my={4} display="flex" justifyContent="space-between">
+        <Box>
+          <Box mb={1}>
+            <Image
+              src="/apolloDAO_logo.png"
+              height={30}
+              alt="Apollo DAO Logo"
+            />
+          </Box>
+          <h1 className="color-primary">xASTRO Lockdrop</h1>
+        </Box>
+        <Box textAlign="center">
+          <Box mb={1}>
+            <h6 className="color-primary">BEGINS IN</h6>
+          </Box>
+          <ApolloCountdown />
         </Box>
       </Box>
     </Box>
