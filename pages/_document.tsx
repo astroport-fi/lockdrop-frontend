c9aa0b546046cca98833bc6c539af3bce5b63ce6
block902 <bruceli829@gmail.com>
2022-04-27 21:49:38 -0400

    feat: add obviously font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <link href="https://use.typekit.net/gtf4unz.css" rel="stylesheet" />
         </Head>
         <body>
           <Main />
